334e2dbd5ea99d94c1b55c5839bfee1f94e50155
Depressedaf1800 <depressedaf1800@gmail.com>
2020-04-02 15:49:46 +0800

    added info but incomplete

--- commands/info/info.js
+++ commands/info/info.js
@@ -1,11 +1,27 @@
-function getMember(message, toFind = ' ') {
-    toFind = toFind.toLowerCase();
-    let target = message.guild.members.cache.fetch(toFind);
-    if(!target && message.mentions.members) target = message.mentions.members.first();
-    if(!target && toFind){
-        target = message.guild.members.cache.find(member => {
-            return member.displayName.toLowerCase().includes(toFind) ||
-                member.user.tag.toLowerCase().includes(toFind)
-        });
+//import libraries
+const {MessageEmbed} = require("discord.js");
+const {stripIndents} = require("common-tags");
+
+// module export format for new commands
+module.exports = {
+    name: "info",
+    aliases: ["user"],
+    category: "info",
+    description: "returns user profile",
+    usage: "[mention | id]",
+    run: async (cat, message, args) => {
+
+        const embed = new MessageEmbed()
+            .setColor("BLUE");
+
+        if(args[0]){
+            return;
+        } else {
+            message.channel.send(embed
+                .setThumbnail(message.author.displayAvatarURL())
+                .setDescription(stripIndents`**USER IDENTITY CARD**\nName: **\`${message.author.username}\`**`)
+                .addField(`${message.author.id}`, "IIIIIIIII IIIII I III IIII II II")
+                .setFooter(`bank-account#${message.author.discriminator}`, cat.user.displayAvatarURL()));
+        }
     }
 }
